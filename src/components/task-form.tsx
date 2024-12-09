3629b2344b3265bbc060b88e0ab00ae35904dbcb
Isai Sanzy <isai10sanchez@gmail.com>
2024-12-09 12:01:44 -0700

    simplified structure of component and made it an actual form element with handleSubmit function

--- src/components/task-form.tsx
+++ src/components/task-form.tsx
@@ -32,7 +32,8 @@ export default function TaskForm({ errors, dispatch }: TaskFormProps) {
   );
   const [seedCount, setSeedCount] = useState<number>(5);
 
-  const handleAddTask = () => {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
     const errors: Partial<TasksState["errors"]> = {};
     if (!taskInput.trim()) {
       errors.task = "Task name is required";
@@ -65,94 +66,92 @@ export default function TaskForm({ errors, dispatch }: TaskFormProps) {
   };
 
   return (
-    <div className="space-y-6">
-      <div className="flex space-x-2">
-        <div className="flex flex-col flex-grow">
-          <Input
-            type="text"
-            value={taskInput}
-            onChange={(e) => {
-              setTaskInput(e.target.value);
-              if (errors.task) {
-                dispatch({
-                  type: "SET_ERROR",
-                  payload: { task: undefined },
-                });
-              }
-            }}
-            placeholder="Enter a new task"
+    <form onSubmit={handleSubmit} className="flex space-x-2">
+      <div className="flex flex-col flex-grow">
+        <Input
+          type="text"
+          value={taskInput}
+          onChange={(e) => {
+            setTaskInput(e.target.value);
+            if (errors.task) {
+              dispatch({
+                type: "SET_ERROR",
+                payload: { task: undefined },
+              });
+            }
+          }}
+          placeholder="Enter a new task"
+          className={cn(
+            errors.task && "border-red-500 focus-visible:ring-red-500",
+          )}
+        />
+        {errors.task && <p className="text-sm text-red-500">{errors.task}</p>}
+      </div>
+      <div className="flex flex-col">
+        <Select
+          value={priorityInput}
+          onValueChange={(value) => {
+            setPriorityInput(value as TaskItem["priority"]);
+            if (errors.priority) {
+              dispatch({
+                type: "SET_ERROR",
+                payload: { priority: undefined },
+              });
+            }
+          }}
+        >
+          <SelectTrigger
             className={cn(
-              errors.task && "border-red-500 focus-visible:ring-red-500",
+              "w-[180px] relative",
+              errors.priority && "border-red-500 focus-visible:ring-red-500",
             )}
-          />
-          {errors.task && <p className="text-sm text-red-500">{errors.task}</p>}
-        </div>
-        <div className="flex flex-col">
-          <Select
-            value={priorityInput}
-            onValueChange={(value) => {
-              setPriorityInput(value as TaskItem["priority"]);
-              if (errors.priority) {
-                dispatch({
-                  type: "SET_ERROR",
-                  payload: { priority: undefined },
-                });
-              }
+          >
+            <SelectValue placeholder="Select priority" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Low">Low</SelectItem>
+            <SelectItem value="Medium">Medium</SelectItem>
+            <SelectItem value="High">High</SelectItem>
+          </SelectContent>
+        </Select>
+        {errors.priority && (
+          <p className="text-sm text-red-500">{errors.priority}</p>
+        )}
+      </div>
+      <Button type="submit" size="icon">
+        <Plus />
+      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="outline" size="icon">
+            <Blocks />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="flex flex-col gap-2 p-4 w-56">
+          <div className="flex w-full space-x-2">
+            <Slider
+              id="seedSlider"
+              min={1}
+              max={20}
+              step={1}
+              value={[seedCount]}
+              onValueChange={(value) => setSeedCount(value[0])}
+            />
+            <Badge variant="secondary">{seedCount}</Badge>
+          </div>
+          <Button
+            className="w-full"
+            onClick={() => {
+              const randomTasks = generateRandomTasks(seedCount);
+              randomTasks.forEach((task) => {
+                dispatch({ type: "ADD_TASK", payload: task });
+              });
             }}
           >
-            <SelectTrigger
-              className={cn(
-                "w-[180px] relative",
-                errors.priority && "border-red-500 focus-visible:ring-red-500",
-              )}
-            >
-              <SelectValue placeholder="Select priority" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="Low">Low</SelectItem>
-              <SelectItem value="Medium">Medium</SelectItem>
-              <SelectItem value="High">High</SelectItem>
-            </SelectContent>
-          </Select>
-          {errors.priority && (
-            <p className="text-sm text-red-500">{errors.priority}</p>
-          )}
-        </div>
-        <Button onClick={handleAddTask} size="icon">
-          <Plus />
-        </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Button variant="outline" size="icon">
-              <Blocks />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="flex flex-col gap-2 p-4 w-56">
-            <div className="flex w-full space-x-2">
-              <Slider
-                id="seedSlider"
-                min={1}
-                max={20}
-                step={1}
-                value={[seedCount]}
-                onValueChange={(value) => setSeedCount(value[0])}
-              />
-              <Badge variant="secondary">{seedCount}</Badge>
-            </div>
-            <Button
-              className="w-full"
-              onClick={() => {
-                const randomTasks = generateRandomTasks(seedCount);
-                randomTasks.forEach((task) => {
-                  dispatch({ type: "ADD_TASK", payload: task });
-                });
-              }}
-            >
-              Add {seedCount} random tasks
-            </Button>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </div>
+            Add {seedCount} random tasks
+          </Button>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </form>
   );
 }
