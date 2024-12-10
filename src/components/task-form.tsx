99d1587b5d56666a010da46d8f5fa95c104b08f9
Isai Sanzy <isai10sanchez@gmail.com>
2024-12-10 10:37:20 -0700

    moved task seeding and filtering components to their own files and imported them here with the appropriate props sent to each one

--- src/components/task-form.tsx
+++ src/components/task-form.tsx
@@ -10,21 +10,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {
-  DropdownMenu,
-  DropdownMenuItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuCheckboxItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-} from "@/components/ui/dropdown-menu";
 import { TaskItem, TasksState, TaskAction } from "@/lib/tasks/types";
 import { cn } from "@/lib/utils";
-import { Slider } from "@/components/ui/slider";
-import { Badge } from "@/components/ui/badge";
-import { Blocks, Plus, Filter } from "lucide-react";
-import { generateRandomTasks } from "@/lib/tasks/utils";
+import { Plus } from "lucide-react";
+import TaskSeeder from "./task-seeder";
+import TaskFilter from "./task-filter";
 
 interface TaskFormProps {
   errors: TasksState["errors"];
@@ -37,25 +27,6 @@ export default function TaskForm({ errors, dispatch, filter }: TaskFormProps) {
   const [priorityInput, setPriorityInput] = useState<TaskItem["priority"] | "">(
     "",
   );
-  const [seedCount, setSeedCount] = useState<number>(5);
-
-  const handleStatusChange = (status: TasksState["filter"]["status"]) => {
-    dispatch({
-      type: "SET_FILTER",
-      payload: { status },
-    });
-  };
-
-  const handlePriorityChange = (priority: TaskItem["priority"]) => {
-    dispatch({
-      type: "SET_FILTER",
-      payload: {
-        priorities: filter.priorities?.includes(priority)
-          ? filter.priorities.filter((p) => p !== priority) // Remove if exists
-          : [...(filter.priorities || []), priority], // Add if doesn't exist
-      },
-    });
-  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -147,115 +118,8 @@ export default function TaskForm({ errors, dispatch, filter }: TaskFormProps) {
         <Button type="submit" size="icon" className="shrink-0">
           <Plus />
         </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              type="button"
-              variant="outline"
-              size="icon"
-              className="shrink-0"
-            >
-              <Blocks />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <div className="flex flex-col gap-2 p-4">
-              <div className="flex w-full space-x-2">
-                <Slider
-                  id="seedSlider"
-                  min={1}
-                  max={20}
-                  step={1}
-                  value={[seedCount]}
-                  onValueChange={(value) => setSeedCount(value[0])}
-                />
-                <Badge variant="secondary">{seedCount}</Badge>
-              </div>
-              <Button
-                className="w-full"
-                type="button"
-                onClick={() => {
-                  const randomTasks = generateRandomTasks(seedCount);
-                  randomTasks.forEach((task) => {
-                    dispatch({ type: "ADD_TASK", payload: task });
-                  });
-                }}
-              >
-                Add {seedCount} random tasks
-              </Button>
-            </div>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              type="button"
-              variant="outline"
-              size="icon"
-              className="shrink-0 relative"
-            >
-              <Filter />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuLabel>Filter Tasks by</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel className="font-normal">
-              Status
-            </DropdownMenuLabel>
-            <DropdownMenuCheckboxItem
-              checked={filter.status === "Completed"}
-              onSelect={(e) => e.preventDefault()}
-              onCheckedChange={() => {
-                if (filter.status === "Completed") {
-                  return;
-                }
-                handleStatusChange("Completed");
-              }}
-            >
-              Completed
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem
-              checked={filter.status === "Active"}
-              onSelect={(e) => e.preventDefault()}
-              onCheckedChange={() => {
-                if (filter.status === "Active") {
-                  return;
-                }
-                handleStatusChange("Active");
-              }}
-            >
-              Active
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel className="font-normal">
-              Priority
-            </DropdownMenuLabel>
-            {(["Low", "Medium", "High"] as const).map((priority) => (
-              <DropdownMenuCheckboxItem
-                key={priority}
-                checked={filter.priorities?.includes(priority)}
-                onSelect={(e) => e.preventDefault()}
-                onCheckedChange={() => handlePriorityChange(priority)}
-              >
-                {priority}
-              </DropdownMenuCheckboxItem>
-            ))}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="flex items-center gap-1 hover:bg-red-500/10 text-red-500"
-              onSelect={(e) => e.preventDefault()}
-              onClick={() => {
-                dispatch({
-                  type: "SET_FILTER",
-                  payload: { priorities: [], status: null },
-                });
-              }}
-            >
-              Clear
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <TaskSeeder dispatch={dispatch} />
+        <TaskFilter filter={filter} dispatch={dispatch} />
       </div>
     </form>
   );
