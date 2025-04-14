fbea80967ad1b80ef792d49e6341e32e59510496
Isai Sanzy <isai10sanchez@gmail.com>
2025-04-14 13:18:48 -0600

    removed all useState declarations and replaced with the state provided by context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useReducer, useState } from "react";
-import { wishlistReducer } from "@/lib/wishes/reducer";
 import { getFilteredWishes } from "@/lib/wishes/utils";
-import { WishlistState } from "@/lib/wishes/types";
-
 import { Card, CardContent } from "@/components/ui/card";
 import { Toaster } from "@/components/ui/toaster";
 import WishlistForm from "@/components/wishes/wishlist-form";
@@ -10,49 +6,21 @@ import BulkActions from "@/components/wishes/bulk-actions";
 import WishlistItem from "@/components/wishes/wishlist-item";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-
-const initialState: WishlistState = {
-  wishItems: [],
-  filter: {
-    priorities: [],
-    status: null,
-  },
-  errors: {},
-  selectedWishes: [],
-};
+import { useWishlist } from "./hooks/use-wishlist";
 
 export default function WishlistApp() {
-  const [state, dispatch] = useReducer(wishlistReducer, initialState);
-
-  const [darkMode, setDarkMode] = useState(false);
-  const [bulkSelectionMode, setBulkSelectionMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    document.documentElement.classList.toggle("dark");
-  };
-
+  const { state } = useWishlist();
   return (
     <div
-      className={`min-h-screen flex flex-col transition-colors ${darkMode ? "dark" : ""}`}
+      className={`bg-background text-foreground min-h-screen flex flex-col transition-colors duration-300 ${state.ui.darkMode ? "dark" : ""}`}
     >
-      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Header />
       <main className="flex-grow">
         <div className="max-w-4xl mx-auto p-4 space-y-4">
-          <WishlistForm
-            errors={state.errors}
-            dispatch={dispatch}
-            filter={state.filter}
-          />
+          <WishlistForm />
 
           <Card>
-            <BulkActions
-              selectedWishes={state.selectedWishes}
-              wishes={state.wishItems}
-              dispatch={dispatch}
-              bulkSelectionMode={bulkSelectionMode}
-              setBulkSelectionMode={setBulkSelectionMode}
-            />
+            <BulkActions />
             <CardContent className="px-4 py-2 space-y-2">
               {state.wishItems.length === 0 ? (
                 <p className="text-center text-muted-foreground mb-4">
@@ -62,14 +30,7 @@ export default function WishlistApp() {
                 <>
                   {getFilteredWishes(state.wishItems, state.filter).map(
                     (wish) => (
-                      <WishlistItem
-                        key={wish.id}
-                        wish={wish}
-                        selectedWishes={state.selectedWishes}
-                        dispatch={dispatch}
-                        bulkSelectionMode={bulkSelectionMode}
-                        setBulkSelectionMode={setBulkSelectionMode}
-                      />
+                      <WishlistItem key={wish.id} wish={wish} />
                     ),
                   )}
                   {getFilteredWishes(state.wishItems, state.filter).length ===
